9b72d133c16ec3f4acae085316b6108b89b20f51
yisakisaac <115116705+yisakisaac@users.noreply.github.com>
2022-10-05 14:28:22 -0700

    update script

--- Weighted Multiple Regression Analysis of Sentiment Toward Gays.R
+++ Weighted Multiple Regression Analysis of Sentiment Toward Gays.R
@@ -1,6 +1,6 @@
 library(poliscidata)
 
-#Question 1 weighted bivariate regression analysis
+#weighted bivariate regression analysis
 as.numeric(nes$pid_x=="partisanship")
 sentiment.towards.gays=svyglm(ftgr_gay~pid_x,design=nesD)
 summary(sentiment.towards.gays)
@@ -8,12 +8,12 @@ fit.svyglm(sentiment.towards.gays)
 svyplot(ftgr_gay~pid_x,design=nesD)
 plot(nes$ftgr_gay~nes$pid_x)
 
-#Question 3 marital status effect on sentiment towards gays
+#marital status effect on sentiment towards gays
 sentiment.towards.gays.marital=svyglm(ftgr_gay~pid_x+married,design=nesD)
 summary(sentiment.towards.gays.marital)
 fit.svyglm(sentiment.towards.gays.marital)
 
-#Question 4 plotting sentiment towards gays
+#plotting sentiment towards gays
 as.numeric(nes$pid_x=="partisanship")
 summary(lm(nes$ftgr_gay~nes$pid_x+nes$married))
 plot(x=nes$pid_x,y=nes$ftgr_gay,xlab="partisanship",ylab="sentiment towards gay")
